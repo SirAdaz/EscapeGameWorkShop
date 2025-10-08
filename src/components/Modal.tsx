5f69525099c643371807fa8a6865079d180672a0
Adaz SirAdaz <mrxptdrgame@gmail.com>
2025-10-08 15:19:23 +0200

    Amélioration du composant Modal : ajout d'un contenu conditionnel pour la valeur "serveur" et ajustement de l'opacité de fond pour une meilleure visibilité.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,15 +8,18 @@ export default function Modal({ isOpen, content, onClose }: ModalProps) {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-base-300 rounded-lg shadow-2xl max-w-2xl w-full mx-4 p-6">
+    <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+      <div className="bg-base-300 bg-black rounded-lg shadow-2xl max-w-2xl w-full mx-4 p-6">
         <h3 className="font-bold text-lg mb-4 text-base-content">
           test
         </h3>
         <div className="whitespace-pre-line text-base-content mb-6">
           {
             content === "disjoncteur" && <div>zizi</div>
-            }
+          }
+          {
+            content === "serveur" && <div>serveur</div>
+          }
         </div>
         <div className="flex justify-end">
           <button
